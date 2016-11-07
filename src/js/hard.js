f641923745a66a94996217307e9b8db07f869e11
Brit Butler <redline6561@gmail.com>
2016-11-07 09:53:19 -0500

    Finish refactoring. Add conjured.

--- src/js/hard.js
+++ src/js/hard.js
@@ -5,49 +5,79 @@ class GildedRose {
     this.quality = quality;
   }
 
-  tick () {
-    if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (this.quality > 0) {
-        if (this.name != 'Sulfuras, Hand of Ragnaros') {
-          this.quality = this.quality - 1;
-        }
-      }
+  normalTick () {
+    this.sellIn--;
+    if (this.quality === 0) {
+      return;
+    }
+
+    if (this.sellIn > 0) {
+      this.quality--;
     } else {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1;
-        if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.sellIn < 11) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-          if (this.sellIn < 6) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-        }
-      }
+      this.quality -= 2;
     }
-    if (this.name != 'Sulfuras, Hand of Ragnaros') {
-      this.sellIn = this.sellIn - 1;
+  }
+
+  agedBrieTick () {
+    this.sellIn--;
+
+    if (this.sellIn <= 0) {
+      this.quality += 2;
+    } else {
+      this.quality += 1;
     }
+
+    if (this.quality > 50) {
+      this.quality = 50;
+    }
+  }
+
+  sulfurasTick () {
+  }
+
+  backstageTick () {
+    this.sellIn--;
+
     if (this.sellIn < 0) {
-      if (this.name != 'Aged Brie') {
-        if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.quality > 0) {
-            if (this.name != 'Sulfuras, Hand of Ragnaros') {
-              this.quality = this.quality - 1;
-            }
-          }
-        } else {
-          this.quality = this.quality - this.quality;
-        }
-      } else {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1;
-        }
-      }
+      this.quality = 0;
+    } else if (this.sellIn < 5) {
+      this.quality += 3;
+    } else if (this.sellIn < 10) {
+      this.quality += 2;
+    } else {
+      this.quality += 1;
+    }
+
+    if (this.quality > 50) {
+      this.quality = 50;
+    }
+  }
+
+  conjuredTick () {
+    this.sellIn--;
+
+    if (this.sellIn > 0){
+      this.quality -= 2;
+    } else if (this.sellIn <= 0) {
+      this.quality -= 4;
+    }
+
+    if (this.quality < 0) {
+      this.quality = 0;
+    }
+  }
+
+  tick () {
+    if (this.name === 'normal') {
+      return this.normalTick();
+    } else if (this.name === 'Aged Brie') {
+      return this.agedBrieTick();
+    } else if (this.name === 'Sulfuras, Hand of Ragnaros') {
+      return this.sulfurasTick();
+    } else if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      return this.backstageTick();
+    } else if (this.name === 'Conjured Mana Cake') {
+      return this.conjuredTick();
     }
   }
 }
